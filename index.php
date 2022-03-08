c53fbf51bc2faa476de79f886904c9da11ad20fe
unknown <da4adindu@gmail.com>
2022-03-08 22:05:23 +0000

    publish

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 include_once("index.html");
 
 
-$file = 'profile.jpg';
-$type = 'image/jpg';
-header('Content-Type:' .$type);
-header('Content-Length: ' . filesize($file));
-readfile($file);
+// $file = 'profile.jpg';
+// $type = 'image/jpg';
+// header('Content-Type:' .$type);
+// header('Content-Length: ' . filesize($file));
+// readfile($file);
 
 ?>
